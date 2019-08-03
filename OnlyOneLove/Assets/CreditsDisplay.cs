3dca051255b0138524d0f9f6d9c3d7d99062858a
DamienKusters <damien.kusters@ziggo.nl>
2019-08-03 16:16:18 +0200

    Add Preferences tab

---
+++ OnlyOneLove/Assets/CreditsDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsDisplay : MonoBehaviour
+{
+    public GameObject Hud;
+    public float timeLeft = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Hud.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ OnlyOneLove/Assets/StatsDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsDisplay : MonoBehaviour
+{
+    public GameObject StatsPanel;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleStats()
+    {
+        if(StatsPanel.active)
+        {
+            StatsPanel.SetActive(false);
+            text.text = "Your Preferences";
+        }
+        else
+        {
+            StatsPanel.SetActive(true);
+            text.text = "Close";
+        }
+    }
+}
